7de78d7036b8447e3004dbb50115d9f6245942f4
yoonisus <yoonisus@naver.com>
2016-05-01 16:28:24 +0000

    please

--- app/controllers/email_controller.rb
+++ app/controllers/email_controller.rb
@@ -1,11 +1,39 @@
+require 'mailgun'
+
 class EmailController < ApplicationController
     def index
-        
     end
+    
     def write
         @apple = params[:title]
         @airplane = params[:content]
-        @cherry = params[:email2]
+        @cherry = params[:email2]   
+        
+        new_post = Post.new
+        new_post.title = @apple
+        new_post.content = @airplane
+        new_post.save 
+    
+        
+        redirect_to "/list"
+        
+        mg_client = Mailgun::Client.new("key-04aa9adcf015d9a729fb8a4b5dd36a4f")
+
+        message_params =  {
+                   from: 'yoonisus@yoonseo.net',
+                   to:   @cherry,
+                   subject: @apple,
+                   text:   @airplane
+                  }
+
+        result = mg_client.send_message('sandbox70e0d65ce89a45ada11bc72d065d8e73.mailgun.org', message_params).to_h!
+
+        message_id = result['id']
+        message = result['message']
         
     end
+    def list
+        @every_post = Post.all.order("id desc")
+    end
+    
 end
